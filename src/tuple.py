9cb0181107a4e74a99b4d5657defb7ca94b69908
belimenko <belimenkonastya@gmail.com>
2023-01-22 21:53:15 +0200

    new

---
+++ src/tuple.py
@@ -0,0 +1,8 @@
+def convert_string(d: tuple, delim: str = ", ") -> str:
+    return delim.join(d)
+
+
+if __name__ == '__main__':
+    tuple1 = ('1', '2', '3', '4', '5', '5', '5', '4', '2',)
+    str1 = convert_string(tuple1)
+    print(str1)
