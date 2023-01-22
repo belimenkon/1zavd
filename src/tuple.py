7afa5f126c87812635016756a26106cc08853d32
belimenko <belimenkonastya@gmail.com>
2023-01-22 22:00:12 +0200

    new

--- src/tuple.py
+++ src/tuple.py
@@ -6,3 +6,11 @@ if __name__ == '__main__':
     tuple1 = ('1', '2', '3', '4', '5', '5', '5', '4', '2',)
     str1 = convert_string(tuple1)
     print(str1)
+
+
+'''The function convert_string is used to convert a tuple to a string
+    Parameters
+    ----------
+    delim : str, optional
+            separator
+'''
